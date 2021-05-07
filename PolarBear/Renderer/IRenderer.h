eba0116e50aa57042ac987c8fae3ba03a71b9b4c
yamahand <a18293372@gmail.com>
2021-05-07 09:54:40 +0900

    d3d12memalloc追加

---
+++ PolarBear/Renderer/IRenderer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace pb{
+
+    struct alignas(64) Renderer{
+        struct DescriptorHeap** pCPUDescriptorHeaps;
+        struct DescriptorHeap** pCbvSrvUavHeaps;
+        struct DescriptorHeap** pSamplerHeaps;
+        class  D3D12MA::Allocator* pResourceAllocator;
+    };
+
+    void InitRenderer();
+}
